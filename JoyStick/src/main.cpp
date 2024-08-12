d24879264916810589aed361fd586b45c53cef7e
GOROman <yoshihito.kondoh@gmail.com>
2024-08-12 21:04:12 +0900

    はじめの九歩: I2C(Wire)でJoyStick基板と通信してみる
    ・I2Cを使うことで、JoyStick基板のボタン状態やスティック状態を取得することができる

--- JoyStick/src/main.cpp
+++ JoyStick/src/main.cpp
@@ -1,7 +1,9 @@
-// はじめの八歩
-// AtomS3のボタンを押している間は白色にする
+// はじめの九歩
+// I2C(Wire)でJoyStick基板と通信してみる
+// I2Cを使うことで、JoyStick基板のボタン状態やスティック状態を取得することができる
 
 #include <Arduino.h>
+#include <Wire.h>  // I2C を使う場合は Wire ライブラリを使う
 
 // フルカラーLEDを使うためのライブラリ
 #include <FastLED.h>
@@ -14,13 +16,6 @@
 
 // Atom JoyStickのLEDの数
 #define NUM_LEDS_JOYSTICK 2
-
-// Atom JoyStickのブザー(G5)
-#define PIN_BUZZER 5
-
-// AtomS3本体のボタン(G41)
-// AtomS3のボタンは画面自体がボタンになっている
-#define PIN_BUTTON 41
 
 // フルカラーLEDの設定
 CRGB leds_joystick[NUM_LEDS_JOYSTICK];
@@ -42,28 +37,58 @@ void setup() {
     // フルカラーLEDの明るさを設定する
     FastLED.setBrightness(32);
 
-    // ブザー用のピンを出力に設定する
-    pinMode(PIN_BUZZER, OUTPUT);
+    gfx.clear();
+    gfx.setCursor(0, 0);
+    gfx.println("Press L/R Button");
 
-    // ボタン用のピンを入力に設定する
-    pinMode(PIN_BUTTON, INPUT);
+    Wire.begin();
 }
 
-void loop() {
-    // ボタンの状態を取得する(押されている場合はLOW)
-    bool button = digitalRead(PIN_BUTTON);
+// Atom JoyStickのI2Cアドレス
+#define I2C_ADDR_ATOM_JOYSTICK 0x59
 
-    if (button == LOW) {
-        // 画面(ボタン)を押している間は白色にする
-        gfx.fillScreen(TFT_WHITE);
-        leds_joystick[0] = CRGB::White;
-        leds_joystick[1] = CRGB::White;
-        FastLED.show();
-    } else {
-        gfx.fillScreen(TFT_NAVY);
-        leds_joystick[0] = CRGB::Black;
-        leds_joystick[1] = CRGB::Black;
-        FastLED.show();
+// Atom JoyStickのボタン状態取得レジスタ
+//   0x70 〜 0x73 までのレジスタにボタン状態が格納されている
+#define I2C_BUTTON_REG 0x70
+
+void loop() {
+    uint8_t data[4] = {0};
+
+    // I2CでAtom JoyStickのボタン状態を取得する
+    {
+        // ボタン状態取得レジスタにアクセスする
+        Wire.beginTransmission(I2C_ADDR_ATOM_JOYSTICK);
+        Wire.write(I2C_BUTTON_REG);
+        Wire.endTransmission(false);
+        // ボタン状態を取得する
+        Wire.requestFrom(I2C_ADDR_ATOM_JOYSTICK, 4);
+        // 4バイト読み込む
+        for (int i = 0; i < 4; i++) {
+            data[i] = Wire.read();
+        }
+        Wire.endTransmission();
     }
+
+    // data[0] が左上のボタンの状態(LOW: 押されている)
+    // data[1] が右上のボタンの状態(LOW: 押されている)
+
+    // 左上のボタンが押された場合は左上LEDを点灯する
+    if (data[0] == LOW) {
+        // 押されている
+        leds_joystick[0] = CRGB::White;
+    } else {
+        // 押されていない
+        leds_joystick[0] = CRGB::Black;
+    }
+
+    // 右上のボタンが押された場合は右上LEDを点灯する
+    if (data[1] == LOW) {
+        leds_joystick[1] = CRGB::White;
+    } else {
+        leds_joystick[1] = CRGB::Black;
+    }
+    // フルカラーLEDの状態を更新する
+    FastLED.show();
+
     delay(10);
 }
